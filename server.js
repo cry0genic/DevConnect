3d94f90c09a04052d8e0de71002fe065673c6554
cry0genic <cry0genic1103@gmail.com>
2021-03-30 14:06:44 +0530

    frontend by quantum

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
 connectDB();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ extended: false }));
-
-app.get('/', (req, res) => {
-  res.send('api running');
-});
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is up on port ${PORT}`);
